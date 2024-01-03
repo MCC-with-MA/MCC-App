39f28b659f7ffe79e4db45d1d1a38e1534523ee5
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2024-01-03 13:45:00 +0530

    Modify basic agent

--- app/src/main/java/mcc/client/agent/AndroidMobileAgent.java
+++ app/src/main/java/mcc/client/agent/AndroidMobileAgent.java
@@ -1,54 +1,29 @@
 package mcc.client.agent;
 
 import jade.core.Agent;
-import jade.core.AgentContainer;
 import jade.core.ContainerID;
 import jade.core.Location;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.lang.acl.ACLMessage;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.util.Logger;
 
 public class AndroidMobileAgent extends Agent {
     private static final long serialVersionUID = 877464243646L;
 
     private transient Logger myLogger = Logger.getJADELogger(getClass().getName());
-    private Location home;
 
     @Override
     public void setup() {
-        home = here();
+        myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Location = "+here().getName());
 
-//        addBehaviour(new TickerBehaviour(this, 10000) {
-//            private static final long serialVersionUID = 87575342545L;
-//
-//            protected void onTick() {
-//                myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Location = "+here().getName()+", tick = "+getTickCount());
-//            }
-//        });
-
-        addBehaviour(new CyclicBehaviour(){
+        addBehaviour(new OneShotBehaviour(){
             private static final long serialVersionUID = 435357658778L;
 
             @Override
             public void action() {
-                ACLMessage msg = receive();
-                if (msg != null) {
-                    myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Move request received");
-                    Location dest = null;
-                    if (here().equals(home)) {
-                        // I'm at home --> move to the Main Container
-                        dest = new ContainerID(AgentContainer.MAIN_CONTAINER_NAME, null);
-                    }
-                    else {
-                        // I'm elsewhere --> go home
-                        dest = home;
-                    }
-                    myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Moving to "+dest.getName());
-                    doMove(dest);
-                }
-                else {
-                    block();
-                }
+                myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Move request received");
+                Location dest = new ContainerID("Container-1", null);
+                myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Moving to "+dest.getName());
+                doMove(dest);
             }
         });
     }
@@ -60,7 +35,6 @@ public class AndroidMobileAgent extends Agent {
 
     @Override
     public void afterMove() {
-        // Restore the logger
         myLogger = Logger.getJADELogger(getClass().getName());
         myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Just arrived in location = "+here().getName());
     }
