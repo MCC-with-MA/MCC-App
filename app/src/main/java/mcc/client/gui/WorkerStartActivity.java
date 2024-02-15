cb3cf05e59477616f9bf1bf55007996c49e2ac74
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2024-02-15 16:59:41 +0530

    Start migrated agent in Worker

--- app/src/main/java/mcc/client/gui/WorkerStartActivity.java
+++ app/src/main/java/mcc/client/gui/WorkerStartActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import jade.android.RuntimeServiceBinder;
+import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
 
 
 public class WorkerStartActivity extends Activity {
@@ -33,6 +36,15 @@ public class WorkerStartActivity extends Activity {
         Button startButton = findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                String containerName = jadeBinder.getContainerHandler().getAgentContainer().getName();
+                Log.i("T",String.valueOf(containerName));
+                try {
+                    AgentController agent = jadeBinder.getContainerHandler().getAgentContainer().getAgent(containerName);
+                    agent.start();
+                } catch (ControllerException e) {
+                    throw new RuntimeException(e);
+                }
+
                 startActivity(new Intent(WorkerStartActivity.this, WorkerRunActivity.class));
             }
         });
