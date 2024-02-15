40fcd924213b8a754ff4f0ab2798b9be6b807a7b
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2024-02-15 17:11:58 +0530

    Fix starting migrated agent

--- app/src/main/java/mcc/client/gui/WorkerStartActivity.java
+++ app/src/main/java/mcc/client/gui/WorkerStartActivity.java
@@ -1,14 +1,18 @@
 package mcc.client.gui;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import jade.android.RuntimeService;
 import jade.android.RuntimeServiceBinder;
 import jade.wrapper.AgentController;
 import jade.wrapper.ControllerException;
@@ -48,5 +52,22 @@ public class WorkerStartActivity extends Activity {
                 startActivity(new Intent(WorkerStartActivity.this, WorkerRunActivity.class));
             }
         });
+
+        Log.i("T", "AndroidMobilityActivity - onCreate()");
+
+        if (jadeBinder == null) {
+            Log.i("T", "AndroidMobilityActivity - JADE binder null. Initialize it");
+            serviceConnection = new ServiceConnection() {
+                public void onServiceConnected(ComponentName className, IBinder service) {
+                    jadeBinder = (RuntimeServiceBinder) service;
+                    Log.i("T", "AndroidMobilityActivity - JADE binder initialized");
+                }
+                public void onServiceDisconnected(ComponentName className) {
+                    jadeBinder = null;
+                    Log.i("T", "AndroidMobilityActivity - JADE binder cleared");
+                }
+            };
+            bindService(new Intent(this, RuntimeService.class), serviceConnection, Context.BIND_AUTO_CREATE);
+        }
     }
 }
